2c9e5d06a06bcd5d12df508ea84bb5ffcc315ac3
ximxim <azim.ahmed7@gmail.com>
2025-07-07 16:45:28 -0400

    Update Docusaurus base URL for deployment
    - Changed the base URL in `docusaurus.config.ts` from '.' to 'podgram-docs' to align with the new deployment strategy.
    - Ensured the configuration is consistent with the current project structure for improved accessibility.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://docs.podgram.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '.',
+  baseUrl: 'podgram-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
