3dd09bc1bcf3db4af146f0169347ca3223718e2d
ximxim <azim.ahmed7@gmail.com>
2025-07-07 16:43:19 -0400

    Update Docusaurus configuration for deployment
    - Changed the production URL to 'https://docs.podgram.ai' for accurate site referencing.
    - Updated the base URL to '.' to align with the new deployment strategy.
    - Ensured configurations are consistent with the current project structure for improved accessibility.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ximxim.github.io',
+  url: 'https://docs.podgram.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/podgram-docs/',
+  baseUrl: '.',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
